7d0ed5531e811de63fd6ca683b99494de8953574
sol <sbuker@itba.edu.ar>
2022-02-16 14:19:01 +0100

    aaa

--- Gemfile
+++ Gemfile
@@ -31,6 +31,8 @@ gem 'font-awesome-sass', '~> 5.6.1'
 
 gem 'simple_form'
 
+gem 'faker'
+
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
+require 'faker'
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.delete_all
+5.times do
+  restaurant = Restaurant.new(name: Faker::Book.title, address: Faker::Address.street_address, phone_number: Faker::PhoneNumber.phone_number, category: ['chinese', 'italian', 'japanese', 'french', 'belgian'].sample)
+  restaurant.save
+end
